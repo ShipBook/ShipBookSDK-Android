09a2156b38a4a2e6070b4e5c83bf3a3a8ed3e7d2
Elisha Sterngold <7370546+elishas@users.noreply.github.com>
2019-09-03 16:49:12 +0300

    upgrade version

--- shipbooksdk/src/main/java/io/shipbook/shipbooksdk/Models/Login.kt
+++ shipbooksdk/src/main/java/io/shipbook/shipbooksdk/Models/Login.kt
@@ -30,7 +30,7 @@ internal data class Login(
         val osVersion: String = Build.VERSION.RELEASE ?: "", // "" for testing
         var appVersion: String = "",
         var appVersionCode: Int = -1,
-        val sdkVersion: String = "1.2.5",
+        val sdkVersion: String = "1.3.0",
         val sdkVersionCode: Int = 1,
         val manufacturer: String = Build.MANUFACTURER ?: "", // "" for testing
         val deviceModel: String = Build.MODEL ?: "", // "" for testing
